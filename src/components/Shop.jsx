82f3ce9375a288d02e6970b9b248ec7ad4fdfb30
John Walter Munene Njeru <104344424+john-walter-munene@users.noreply.github.com>
2026-02-10 09:29:23 +0300

    Fix import casing for Linux deployment

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
 import styles from "../assets/styles/Shop.module.css";
 import { formatNumber, countItemsRecursive, trimByWords } from "./utils";
-import { NavBar } from "./NavBar.jsx";
+import { NavBar } from "./Navbar.jsx";
 import { Footer } from "./Footer.jsx";
 
 function ShoppingPage({ products, setProducts, cart, setCart }) {
